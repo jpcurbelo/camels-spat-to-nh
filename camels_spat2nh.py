39191251a217a9e5d5fc9a2ae0c44b34b280b6a5
ame805 <ame805@mail.usask.ca>
2024-02-06 13:45:24 -0600

    Prepared script to be run on Copernicus

---
+++ camels_spat2nh.py
@@ -0,0 +1,255 @@
+import os
+import sys
+import xarray as xr
+import pandas as pd
+import yaml
+import time
+
+from functools import partial
+from concurrent.futures import ThreadPoolExecutor
+
+from utils.utils import reduceDataByDay
+
+# Get the current working directory of the notebook
+current_dir = os.getcwd()
+# Add the parent directory of the notebook to the Python path
+# root_dir = os.path.abspath(os.path.join(current_dir, '..', '..'))
+root_dir = os.path.abspath(current_dir)
+sys.path.append(root_dir)
+
+
+def load_util_data():
+    
+    # **Load Data**
+    data_dir_path = os.path.join(root_dir, "utils", "data_dir.yml")
+    with open(data_dir_path, 'r') as ymlfile:
+        data_dir = yaml.load(ymlfile, Loader=yaml.FullLoader)
+    
+    data_gen_path = os.path.join(root_dir, "utils", "data_general.yml")
+    with open(data_gen_path, 'r') as ymlfile:
+        data_gen = yaml.load(ymlfile, Loader=yaml.FullLoader)
+               
+    return data_dir, data_gen
+
+def camels_spat2nh(data_dir, data_gen):
+
+    # **Load Data**
+    ## Dirs data
+    data_dir_src = data_dir['data_dir_camels_spat']
+    data_dir_out = data_dir['data_dir_camels_spat_nh']
+    relative_path_forc = data_dir['relative_path_forcing']
+    relative_path_targ = data_dir['relative_path_target']
+    ## Basins data
+    basin_data_path = os.path.join(data_dir_src, 'basin_data')
+    list_basin_files = sorted(os.listdir(basin_data_path))
+    
+    ## General data
+    countries = data_gen['countries']
+    data_sources = data_gen['data_sources']
+    
+    # Filter folders by country name (3 first letters) - create a dictionary
+    basin_data_path_dict = {}
+    for country in countries:
+        basin_data_path_dict[country] = [basin for basin in list_basin_files if basin[:3] == country]
+    
+    ## Process data for each basin and save to csv file
+    for country in countries:
+        
+        # Create a folder for each country
+        country_dir = os.path.join(data_dir_out, f'CAMELS_spat_{country}')
+        if not os.path.exists(country_dir ):
+            os.makedirs(country_dir)
+        # else:
+        #     print(f"Directory {country_dir} already exists")
+            
+        for basin_f in basin_data_path_dict[country]:
+            print('\n', basin_f)
+            ## Load input data
+            df_src_dict = {}
+            for src in data_sources:
+                
+                folder2load = os.path.join(basin_data_path, basin_f, relative_path_forc)
+                eras_files = sorted([f for f in os.listdir(folder2load) if src in f])
+                
+                # Initialize an empty list to store the xarray datasets
+                datasets = []
+                # Iterate over the files and load each dataset
+                for file2load in eras_files:
+                    
+                    # If not .temp file
+                    if '.tmp' not in file2load:
+                        
+                        sys.stdout.write(f'\r>> {file2load}')
+                        sys.stdout.flush()
+                        
+                        basin_data = xr.open_dataset(os.path.join(folder2load, file2load))
+                        datasets.append(basin_data)
+                    
+                # Concatenate all datasets along the 'time' dimension
+                concatenated_dataset = xr.concat(datasets, dim='time')
+                    
+                # Reduce basin_data to daily values
+                basin_data_reduced = reduceDataByDay(concatenated_dataset, data_gen['input_vars'], data_gen['sum_vars'])
+                # Convert the reduced basin_data to a DataFrame, dropping the 'hru' dimension
+                basin_data_df = basin_data_reduced.to_dataframe().droplevel('hru').reset_index()
+                
+                # Save to dict
+                df_src_dict[src] = basin_data_df
+                
+            # Merge dataframes in the dictionary
+            df_merged_inp = df_src_dict[data_sources[0]].merge(df_src_dict[data_sources[1]], on='time')
+            
+            # Rename time by date
+            df_merged_inp.rename(columns={'time': 'date'}, inplace=True)
+            
+            ## Load target data
+            target_data = xr.open_dataset(os.path.join(basin_data_path, basin_f, relative_path_targ, f'{basin_f}_daily_flow_observations.nc'))
+            
+            # Subset by data_gen['target_vars']
+            target_data = target_data[data_gen['target_vars']]
+            # Convert to DataFrame
+            df_target = target_data.to_dataframe().reset_index()
+            # Rename time by date
+            df_target.rename(columns={'time': 'date'}, inplace=True)
+            # Remove duplicates
+            df_target = df_target.drop_duplicates(subset=['date'])
+            
+            # Merge input and target dataframes
+            df_merged = df_merged_inp.merge(df_target, on='date')
+            
+            # Save to file
+            df_merged.to_csv(os.path.join(country_dir, basin_f + '.csv'), index=False)
+
+
+
+def camels_spat2nh_parallel(data_dir, data_gen):
+
+    # **Load Data**
+    ## Dirs data
+    data_dir_src = data_dir['data_dir_camels_spat']
+    data_dir_out = data_dir['data_dir_camels_spat_nh']
+    relative_path_forc = data_dir['relative_path_forcing']
+    relative_path_targ = data_dir['relative_path_target']
+    ## Basins data
+    basin_data_path = os.path.join(data_dir_src, 'basin_data')
+    list_basin_files = sorted(os.listdir(basin_data_path))
+    
+    ## General data
+    countries = data_gen['countries']
+    data_sources = data_gen['data_sources']
+    
+    # Filter folders by country name (3 first letters) - create a dictionary
+    basin_data_path_dict = {}
+    for country in countries:
+        basin_data_path_dict[country] = [basin for basin in list_basin_files if basin[:3] == country]
+
+
+    # # Process data for each basin and save to csv file
+    # for country in data_gen['countries']:
+    #     country_dir = os.path.join(data_dir_out, f'CAMELS_spat_{country}')
+    #     if not os.path.exists(country_dir):
+    #         os.makedirs(country_dir)
+
+    #     for basin_f in basin_data_path_dict[country]:
+    #         print('\n', basin_f)
+    #         # Use ThreadPoolExecutor for parallel processing of each basin
+    #         with ThreadPoolExecutor() as executor:
+    #             # future = executor.submit(process_basin, data_dir, data_gen, country, basin_f)
+    #             future = executor.submit(process_basin, data_gen, basin_f, data_sources, relative_path_forc, 
+    #                                      relative_path_targ, basin_data_path, country_dir)
+    #             future.result()  # Wait for the task to complete before moving to the next basin
+    
+    # Use ThreadPoolExecutor for parallel processing of each basin
+    with ThreadPoolExecutor() as executor:
+        futures = []
+        for country in data_gen['countries']:
+            country_dir = os.path.join(data_dir_out, f'CAMELS_spat_{country}')
+            if not os.path.exists(country_dir):
+                os.makedirs(country_dir)
+
+            for basin_f in basin_data_path_dict[country]:
+                future = executor.submit(process_basin, data_gen, basin_f, data_sources, relative_path_forc, 
+                                         relative_path_targ, basin_data_path, country_dir)
+                futures.append(future)
+
+        # Wait for all tasks to complete
+        for future in futures:
+            future.result()
+
+def process_basin(data_gen, basin_f, data_sources, relative_path_forc, relative_path_targ, 
+                  basin_data_path, country_dir):
+
+        ## Load input data
+        df_src_dict = {}
+        for src in data_sources:
+            
+            folder2load = os.path.join(basin_data_path, basin_f, relative_path_forc)
+            eras_files = sorted([f for f in os.listdir(folder2load) if src in f])
+            
+            # Initialize an empty list to store the xarray datasets
+            datasets = []
+            # Iterate over the files and load each dataset
+            for file2load in eras_files[:50]:
+                
+                sys.stdout.write(f'\r>> {file2load}')
+                sys.stdout.flush()
+                
+                # If not .temp file
+                if '.tmp' not in file2load:
+                    basin_data = xr.open_dataset(os.path.join(folder2load, file2load))
+                    datasets.append(basin_data)
+                
+            # Concatenate all datasets along the 'time' dimension
+            concatenated_dataset = xr.concat(datasets, dim='time')
+                
+            # Reduce basin_data to daily values
+            basin_data_reduced = reduceDataByDay(concatenated_dataset, data_gen['input_vars'], data_gen['sum_vars'])
+            # Convert the reduced basin_data to a DataFrame, dropping the 'hru' dimension
+            basin_data_df = basin_data_reduced.to_dataframe().droplevel('hru').reset_index()
+            
+            # Save to dict
+            df_src_dict[src] = basin_data_df
+            
+        # Merge dataframes in the dictionary
+        df_merged_inp = df_src_dict[data_sources[0]].merge(df_src_dict[data_sources[1]], on='time')
+        
+        # Rename time by date
+        df_merged_inp.rename(columns={'time': 'date'}, inplace=True)
+        
+        ## Load target data
+        target_data = xr.open_dataset(os.path.join(basin_data_path, basin_f, relative_path_targ, f'{basin_f}_daily_flow_observations.nc'))
+        
+        # Subset by data_gen['target_vars']
+        target_data = target_data[data_gen['target_vars']]
+        # Convert to DataFrame
+        df_target = target_data.to_dataframe().reset_index()
+        # Rename time by date
+        df_target.rename(columns={'time': 'date'}, inplace=True)
+        # Remove duplicates
+        df_target = df_target.drop_duplicates(subset=['date'])
+        
+        # Merge input and target dataframes
+        df_merged = df_merged_inp.merge(df_target, on='date')
+        
+        # Save to file
+        df_merged.to_csv(os.path.join(country_dir, basin_f + '.csv'), index=False)
+
+
+if __name__ == '__main__':
+    
+
+    # camels_spat2nh()
+    data_dir, data_gen = load_util_data()
+    
+    # # ## Let's profile the loop
+    # # start_time = time.time()
+    # # camels_spat2nh_parallel(data_dir, data_gen)
+    # # ## End of process
+    # # print('\n', f"--- {(time.time() - start_time):.2f} seconds ---")
+    
+    ## Let's profile the loop
+    start_time = time.time()
+    camels_spat2nh(data_dir, data_gen)
+    ## End of process
+    print('\n', f"--- {(time.time() - start_time):.2f} seconds ---")
+    
